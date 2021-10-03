807e8fd2e3200e6aad18e882b6680b85fd3f8fa6
Xuan Li <mowangblog@qq.com>
2021-10-03 18:33:00 +0800

    ✨update

--- JavaWeb_servlet/src/main/java/com/example/JavaWeb_servlet/ServletContextDemo.java
+++ JavaWeb_servlet/src/main/java/com/example/JavaWeb_servlet/ServletContextDemo.java
@@ -4,6 +4,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.WeakHashMap;
 
 /**
  * ServletContextDemo
@@ -15,15 +17,15 @@ import java.io.IOException;
 @WebServlet(name = "ServletContextDemo", value = "/ServletContextDemo")
 public class ServletContextDemo extends HttpServlet {
 
-    /*
-        servlet
-
-     */
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletContext servletContext = getServletContext();
-
+        System.out.println("servletContext.getInitParameter(\"name\") = " + servletContext.getInitParameter("name"));
+        System.out.println("servletContext.getInitParameter(\"url\") = " + servletContext.getInitParameter("url"));
+        System.out.println("获得工程路径" + servletContext.getContextPath());
+        System.out.println("获得工程部署路径" + servletContext.getRealPath("/WEB-INF"));
+        servletContext.setAttribute("user","mowang");
+        System.out.println("servletContext.getAttribute(\"user\") = " + servletContext.getAttribute("user"));
     }
 
     @Override
--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# JavaWeb
-## ✨Java Web 知识点
+# ✨JavaWeb
+## Java Web 知识点
 
 1. [XML](#Xml)
-   - [xml 的主要作用](#xml 的主要作用)
-   - [xml 的语法](#xml 的主要作用)
+   - [xmlGrammar](#xmlGrammar)
 2. [Servlet](#Servlet)
-   - [什么是 Servlet](#什么是 Servlet )
    - [ServletConfig](#ServletConfig) 
    - [ServletContext](#ServletContext)
 3. Filter
@@ -18,13 +16,11 @@
 
 ## Xml
 
-### xml 的主要作用
+1. 用来保存数据，而且这些数据具有自我描述性 
+2. 它还可以做为项目或者模块的配置文件 
+3. 还可以做为网络传输数据的格式（现在一般使用JSON)
 
-- 用来保存数据，而且这些数据具有自我描述性 
-- 它还可以做为项目或者模块的配置文件 
-- 还可以做为网络传输数据的格式（现在一般使用JSON)
-
-### xml 的语法
+### xmlGrammar
 
 - 文档声明。 
 - 元素（标签） 
@@ -34,11 +30,9 @@
 
 ## Servlet
 
-### 什么是 Servlet 
-
-- Servlet 是 JavaEE 规范之一。规范就是接口 
-- Servlet 就 JavaWeb 三大组件之一。三大组件分别是：Servlet 程序、Filter 过滤器、Listener 监听器。 
-- Servlet 是运行在服务器上的一个 java 小程序，它可以接收客户端发送过来的请求，并响应数据给客户端。
+1. Servlet 是 JavaEE 规范之一。规范就是接口 
+2. Servlet 就 JavaWeb 三大组件之一。三大组件分别是：Servlet 程序、Filter 过滤器、Listener 监听器。 
+3. Servlet 是运行在服务器上的一个 java 小程序，它可以接收客户端发送过来的请求，并响应数据给客户端。
 
 ### ServletConfig
 
